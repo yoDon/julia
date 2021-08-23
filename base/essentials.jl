4b119217db0b252fb48158ccdeadb0ce9e789187
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2021-08-23 23:02:03 +0900

    Update base/essentials.jl
    Co-authored-by: Tim Holy <tim.holy@gmail.com>

--- base/essentials.jl
+++ base/essentials.jl
@@ -54,8 +54,8 @@ end
     @nospecialize
 
 Applied to a function argument name, hints to the compiler that the method
-should not be specialized for different types of that argument,
-but instead to use precisely the declared type for each argument.
+implementation should not be specialized for different types of that argument,
+but instead use the declared type for that argument.
 Can be applied to an argument within a formal argument list,
 or in the function body.
 When applied to an argument, the macro must wrap the entire argument expression.
